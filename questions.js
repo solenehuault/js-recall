5b33cf5414e3eddd3508a1346fd325058eff403a
solenehuault <solene.huault@gmail.com>
2016-07-12 14:14:01 +0200

    spec 17 done

--- questions.js
+++ questions.js
@@ -132,7 +132,7 @@ var repeatElements = function(array) {
 }
 
 var stringToNumber = function(string) {
-    return 'Write your method here';
+    return parseInt(string);
 }
 
 var calculateAverage = function(array) {
